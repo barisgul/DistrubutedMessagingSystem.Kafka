a491e8acd661e4ecee2b72d7e62b2d2f7a256f5b
barisgul <bariss.gull@gmail.com>
2021-02-06 18:32:13 +0300

    DistrubutedMessagingSystem.Kafka project created

---
+++ src/DistrubutedMessagingSystem.KafkaProducer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DistrubutedMessagingSystem.KafkaProducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
