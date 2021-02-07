4dec3a0f98030bc2a45bb9583e12af6fc783ef83
barisgul <bariss.gull@gmail.com>
2021-02-08 01:02:16 +0300

    consumer added

---
+++ src/DistrubutedMessagingSystem.KafkaConsumer/Consumer.cs
@@ -0,0 +1,74 @@
+﻿
+using Confluent.Kafka;
+using DistrubutedMessagingSystem.Domain.Entities;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace DistrubutedMessagingSystem.KafkaConsumer
+{
+    public class Consumer
+    {
+        IConfigurationRoot configurationRoot;
+        public Consumer(IConfigurationRoot configurationRoot)
+        {
+            this.configurationRoot = configurationRoot;
+        }
+        public void Consume()
+        {
+            string serverAddress = configurationRoot.GetSection("Kafka:ServerAddress").Get<string>();
+            string topic = configurationRoot.GetSection("Kafka:Topic").Get<string>();
+            string port = configurationRoot.GetSection("Kafka:Port").Get<string>();
+
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = serverAddress + ":" + port.Trim(),
+                AutoOffsetReset = AutoOffsetReset.Earliest,
+                GroupId = "my-consumer-group"
+            };
+
+            using (var c = new ConsumerBuilder<Null, string>(config).Build())
+            {
+                c.Subscribe(topic);
+
+                CancellationTokenSource cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) =>
+                {
+                    e.Cancel = true;
+                    cts.Cancel();
+                };
+
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var cr = c.Consume(cts.Token);
+                            City city = JsonConvert.DeserializeObject<City>(cr.Value);
+                            Console.WriteLine(string.Format("Delivery succeeded! City is {0} at {1}", city.Name, cr.TopicPartitionOffset)); 
+                        }
+                        catch (ConsumeException ex)
+                        {
+                            Console.WriteLine($"Error occured: {ex.Error.Reason}");
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Error occured: {ex.ToString()}");
+                        }
+                    }
+                    c.Commit();
+
+                }
+                catch(OperationCanceledException ex)
+                {
+                    Console.WriteLine("Error: ",ex.Message);
+                }
+            }
+        }
+    }
+}
+
---
+++ src/DistrubutedMessagingSystem.KafkaConsumer/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace DistrubutedMessagingSystem.KafkaConsumer
+{
+    class Program
+    {
+        static IConfigurationRoot configurationRoot;
+        static void Main(string[] args)
+        {
+            configurationRoot = Configure();
+            Consumer consumer = new Consumer(configurationRoot);
+            consumer.Consume();
+        }
+
+        private static IConfigurationRoot Configure()
+        {
+            // Build configuration
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+
+            return config;
+        }
+    }
+}
